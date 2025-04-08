09812835e09b2b39b41e4dfaf0141b6f745a03df
Riffatad <riffatad76@gmail.com>
2025-04-07 21:27:25 -0400

    2nd commit

--- IRIS.sql
+++ IRIS.sql
@@ -1,26 +1,28 @@
+-- drop table if table already created
 Drop TABLE iris_data
 
+-- Creating a table
 CREATE TABLE iris_data (
-    
-    sepal_length REAL NOT NULL,
-    sepal_width REAL NOT NULL,
-    petal_length REAL NOT NULL,
-    petal_width REAL NOT NULL,
-    species VARCHAR NOT NULL
+sepal_length REAL NOT NULL,
+sepal_width REAL NOT NULL,
+petal_length REAL NOT NULL,
+petal_width REAL NOT NULL,
+species VARCHAR NOT NULL,
 );
 
-SELECT * FROM iris_data 
+-- displaying table data
+SELECT * FROM iris_data
 
--- Retrieve the first 10 rows
-SELECT * 
+-- Retrieve the first 15 rows
+SELECT *
 FROM iris_data
-LIMIT 10;
+LIMIT 15;
 
--- Find the average 'petal_width' by species
+-- the average 'petal_width' by species
 SELECT species, AVG(petal_width) AS avg_petal_width
 FROM iris_data
 GROUP BY species;
 
-
+-- Correlation between 'sepal_length' and 'petal_length'
 SELECT CORR(sepal_length, petal_length) AS correlation
 FROM iris_data;
