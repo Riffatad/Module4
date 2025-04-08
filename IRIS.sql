09879f98bf364032af7b307f7ad73c8af5d9e7b7
Riffatad <riffatad76@gmail.com>
2025-04-07 21:50:16 -0400

    3rd commit

--- IRIS.sql
+++ IRIS.sql
@@ -10,19 +10,3 @@ petal_width REAL NOT NULL,
 species VARCHAR NOT NULL,
 );
 
--- displaying table data
-SELECT * FROM iris_data
-
--- Retrieve the first 15 rows
-SELECT *
-FROM iris_data
-LIMIT 15;
-
--- the average 'petal_width' by species
-SELECT species, AVG(petal_width) AS avg_petal_width
-FROM iris_data
-GROUP BY species;
-
--- Correlation between 'sepal_length' and 'petal_length'
-SELECT CORR(sepal_length, petal_length) AS correlation
-FROM iris_data;
